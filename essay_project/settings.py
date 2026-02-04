0215309ab7ea1a3cdd14289b92757a50ff047967
Sachin Trokchhaki Magar <157393885+SachinTrokchhaki@users.noreply.github.com>
2026-02-04 20:13:02 +0545

    Update settings.py

--- essay_project/settings.py
+++ essay_project/settings.py
@@ -128,12 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_DIRS=[
-#     BASE_DIR / 'static',
-# ]
-# STATIC_ROOT= BASE_DIR / 'static'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = []
@@ -154,4 +148,5 @@ LOGIN_REDIRECT_URL = '/'  # Where to redirect after successful login
 LOGOUT_REDIRECT_URL = '/'  # Where to redirect after logout
 
 # For development only - emails will print to console
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
